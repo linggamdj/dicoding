d2dcb3e8f86684418c893bcdfa850f8afa9979fb
Lingga <linggaadiatmdj@gmail.com>
2022-03-04 23:13:09 +0700

    data validation added

--- notes-app-back-end/src/api/notes/handler.js
+++ notes-app-back-end/src/api/notes/handler.js
@@ -1,3 +1,5 @@
+const ClientError = require("../../exceptions/ClientError");
+
 class NotesHandler {
     constructor(service, validator) {
         this._service = service;
@@ -28,11 +30,22 @@ class NotesHandler {
             response.code(201);
             return response;
         } catch (error) {
-            const response = h.response({
-                status: 'fail',
-                message: error.message,
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.request({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(400);
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
@@ -58,11 +71,22 @@ class NotesHandler {
                 },
             };
         } catch (error) {
-            const response = h.response({
-                status: 'fail',
-                message: error.message,
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.request({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(404);
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
@@ -79,11 +103,22 @@ class NotesHandler {
                 message: 'Catatan berhasil diperbarui',
             }
         } catch (error) {
-            const response = h.response({
-                status: 'fail',
-                message: error.message,
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.request({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(404);
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
@@ -97,11 +132,22 @@ class NotesHandler {
                 message: 'Catatan berhasil dihapus',
             }
         } catch (error) {
-            const response = h.response({
-                status: 'fail',
-                message: 'Catatan gagal dihapus. Id tidak ditemukan',
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message,
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server Error
+            const response = h.request({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
             });
-            response.code(404);
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
