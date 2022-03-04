c5379d9d0da155f7cc1d1801cca19940841760f4
Lingga <linggaadiatmdj@gmail.com>
2022-03-04 22:54:01 +0700

    adding validator

--- notes-app-back-end/src/api/notes/handler.js
+++ notes-app-back-end/src/api/notes/handler.js
@@ -1,6 +1,7 @@
 class NotesHandler {
-    constructor(service) {
+    constructor(service, validator) {
         this._service = service;
+        this._validator = validator;
 
         this.postNoteHandler = this.postNoteHandler.bind(this);
         this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,10 +12,10 @@ class NotesHandler {
 
     postNoteHandler(request, h) {
         try {
+            this._validator.validateNotePayload(request.payload);
             const {title = 'untitled', body, tags} = request.payload;
 
             this._service.addNote({title, body, tags});
-    
             const noteId = this._service.addNote({title, body, tags});
     
             const response = h.response({
@@ -68,6 +69,7 @@ class NotesHandler {
 
     putNoteByIdHandler(request, h) {
         try {
+            this._validator.validateNotePayload(request.payload);
             const {id} = request.params;
 
             this._service.editNoteById(id, request.payload);
