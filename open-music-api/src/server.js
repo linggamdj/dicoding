07b6dfdddb053f35e6995292f48d6915265e00de
Lingga <linggaadiatmdj@gmail.com>
2022-03-13 21:56:02 +0700

    user registration implemented

--- open-music-api/src/server.js
+++ open-music-api/src/server.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const albums = require('./api/albums');
-const songs = require('./api/songs');
+
 const ClientError = require('./exceptions/ClientError');
-const AlbumsService = require('./services/postgres/AlbumsService');
+
+// songs
+const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
-const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+
+// albums
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -36,6 +47,13 @@ const init = async () => {
       options: {
         service: albumsService,
         validator: AlbumsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
